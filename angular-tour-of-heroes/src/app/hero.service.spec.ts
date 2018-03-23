00b8af7dd8bd773d2f01993098fada761f7863d2
lucassveloso <lucas.s.veloso@hotmail.com>
2018-03-23 10:38:01 -0300

    Criando exemplo de teste de service no angular

--- angular-tour-of-heroes/src/app/hero.service.spec.ts
+++ angular-tour-of-heroes/src/app/hero.service.spec.ts
@@ -1,15 +1,58 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 import { HeroService } from './hero.service';
+import { MessageService } from './message.service';
+import { Hero } from './hero';
 
 describe('HeroService', () => {
+  const apiUrl = 'http://localhost:4000';  // URL to web api
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+  let heroService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [HeroService]
+      imports: [HttpClientTestingModule],
+      providers: [HeroService, MessageService]
     });
+
+    heroService = TestBed.get(HeroService);
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
   });
 
   it('should be created', inject([HeroService], (service: HeroService) => {
     expect(service).toBeTruthy();
+  }));
+
+  it('should be return heroes', inject([HeroService], (service: HeroService) => {
+    const testData: Hero[] = [
+      {
+        id: 1,
+        name: 'teste'
+      }
+    ];
+
+    // Make an HTTP GET request
+    heroService.getHeroes().subscribe(heroes =>
+      expect(heroes).toEqual(testData)
+    );
+
+    // The following `expectOne()` will match the request's URL.
+    // If no requests or multiple requests matched that URL
+    // `expectOne()` would throw.
+    const req = httpTestingController.expectOne(`${apiUrl}/heroes`);
+
+    // Assert that the request is a GET.
+    expect(req.request.method).toEqual('GET');
+
+    // Respond with mock data, causing Observable to resolve.
+    // Subscribe callback asserts that correct data was returned.
+    req.flush(testData);
+
+    // Finally, assert that there are no outstanding requests.
+    httpTestingController.verify();
   }));
 });
